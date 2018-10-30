50eec1ffa78bea5763eb1520b37703033f7670a3
Unknown <krys.nagorny@gmail.com>
2018-10-31 00:21:39 +0100

    last build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,22 @@ import {ContentService} from './services/content.service';
 import {HttpClientModule} from '@angular/common/http';
 import {ProjectsComponent} from './components/projects/projects.component';
 import {SortingComponent} from './components/projects/sorting/sorting.component';
+import {AboutComponent} from './pages/about/about.component';
+import {routes} from './app.routers';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProjectsComponent,
-    SortingComponent
+    SortingComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [LanguageService, ContentService],
   bootstrap: [AppComponent]
---
+++ src/app/app.routers.ts
@@ -0,0 +1,7 @@
+import {Route} from '@angular/router';
+import {AboutComponent} from './pages/about/about.component';
+
+export const routes: Route[] = [
+  { path: 'about', component: AboutComponent},
+
+];
