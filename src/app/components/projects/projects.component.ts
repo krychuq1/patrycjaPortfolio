fb538b344046dc7ed48041dfc1970fcfc48f4443
Unknown <krys.nagorny@gmail.com>
2018-11-12 23:40:26 +0100

    + add hover to project to thumbnail

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {LanguageService} from '../../services/language.service';
 import {SortingService} from '../../services/sorting.service';
+import {ContentService} from '../../services/content.service';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.html',
@@ -8,11 +9,17 @@ import {SortingService} from '../../services/sorting.service';
 })
 export class ProjectsComponent {
   filter: string;
-  constructor(private languageService: LanguageService, private sortingService: SortingService) {
+  contentUrl: string;
+  content: object;
+  constructor(private languageService: LanguageService,
+              private sortingService: SortingService, private contentService: ContentService) {
     this.filter = 'any';
-    // this.languageService.changeLanguage.subscribe(() => {
-    //   this.getContent();
-    // });
+    this.contentUrl = 'component/projects/';
+    this.getContent();
+
+    this.languageService.changeLanguage.subscribe(() => {
+      this.getContent();
+    });
     this.sortingService.sortingEven.subscribe((val) => {
       this.filter = val;
     });
@@ -23,5 +30,12 @@ export class ProjectsComponent {
   public setLanguage(lan: string) {
     this.languageService.setLanguage(lan);
     // this.getContent();
+  }
+  getContent() {
+    this.contentService.getContent(this.contentUrl).then((content) => {
+      this.content = content;
+    }, err => {
+      console.error(err);
+    });
   }
 }
